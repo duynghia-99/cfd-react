2ee890f0cb58e9fe0e69da1ff500b2ffa2b8d8dc
Tran Nghia <nghiatran@techvsi.com>
2021-05-05 17:58:36 +0700

    add page 404, introduce-coin, cooperate, email, faq and profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { Footer, Header } from './component';
+import Cooperate from './page/cooperate';
 import Course from './page/course';
 import CourseDetail from './page/course-detail';
+import Email from './page/email';
+import FagPage from './page/faqpage';
 import Home from './page/home';
+import Error from './page/home/404';
+import IntroduceCoin from './page/introduce-coin';
+import Profile from './page/profile';
 
 function App() {
   return (
@@ -9,7 +15,13 @@ function App() {
       <Header/>
       {/* <Home/> */}
       {/* <Course/> */}
-      <CourseDetail/>
+      {/* <CourseDetail/> */}
+      {/* <FagPage/> */}
+      {/* <Error/> */}
+      {/* <Email/> */}
+      {/* <IntroduceCoin/> */}
+      {/* <Cooperate/> */}
+      <Profile/>
       <Footer/>
     </div>
   );
